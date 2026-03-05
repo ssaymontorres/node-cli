41f6378bc1374c1bb040dd70d252f19c15c73012
ssaymontorres <saymontorres48@gmail.com>
2026-03-05 15:59:43 -0300

    fix:multiletask persistence

--- index.ts
+++ index.ts
@@ -1,26 +1,31 @@
-//chama o modulo nativo File Sytem e importa a forma moderna(promises)
 import fs from 'node:fs/promises';
+//chama o modulo nativo File Sytem e importa a forma moderna(promises)
 
-//define a interface da tarefa 
 interface Tarefa {
     id: number;
     titulo: string;
     concluido: boolean;
 }
+//define a interface da tarefa 
 
 async function adicionarTarefa(novaTarefa: Tarefa) {
     let listaAtual: Tarefa[] = [];
 
     try {
-        // tenta ler o arquivo pra não sobrescrever dados antigos
+        // tenta ler o arquivo para não sobrescrever dados antigos
         const dadosBrutos = await fs.readFile('tarefa.json', 'utf-8');
-        listaAtual = JSON.parse(dadosBrutos);
-    } catch (erro) {
-        // se o arquivo não existir sehgue com a lista vazia
-        console.log('criando novo banco de dados local');
-    }
+        const dadosTraduzidos = JSON.parse(dadosBrutos);
 
-    // add o novo objeto a array
+        // garante que o que foi lido é realmente uma lista
+        if (Array.isArray(dadosTraduzidos)) {
+            listaAtual = dadosTraduzidos;
+        }
+    } catch (erro) {
+        // Se o arquivo não existir segue com a lista vazia
+        console.log('criando novo banco de dados local');
+    } 
+
+    // adiciona o novo objeto ao Array
     listaAtual.push(novaTarefa);
 
     const conteudo = JSON.stringify(listaAtual, null, 2);
@@ -28,7 +33,7 @@ async function adicionarTarefa(novaTarefa: Tarefa) {
 
     try {
         await fs.writeFile('tarefa.json', conteudo);
-        console.log(`tarefa "${novaTarefa.titulo}" gravada com sucesso`);
+        console.log(`Tarefa "${novaTarefa.titulo}" gravada com sucesso`);
     }
     //tenta escrever o arquivo tarefa.json
     catch (erro) {
@@ -37,21 +42,28 @@ async function adicionarTarefa(novaTarefa: Tarefa) {
     //se der errado cai aqui
 }
 
-// criando exemplos para testar o Array
+//exemplos para testar o array
 const tarefa1: Tarefa = {
     id: 1,
-    titulo: "Aprender Node",
+    titulo: "make todo in node",
     concluido: true
 };
 
 const tarefa2: Tarefa = {
     id: 2,
-    titulo: "Subir no GitHub",
+    titulo: "commit in git",
+    concluido: true
+};
+
+const tarefa3: Tarefa = {
+    id: 3,
+    titulo: "make readme",
     concluido: false
 };
 
-// func autoinvocável para rodar as gravações em sequencia
+// Função autoinvocavel para rodar as gravçoes em sequencia
 (async () => {
     await adicionarTarefa(tarefa1);
     await adicionarTarefa(tarefa2);
+    await adicionarTarefa(tarefa3); 
 })();
